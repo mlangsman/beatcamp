962a60cc1ff455b8e5fee383ff1cead22ba724e6
Marc <marc@langsman.com>
2018-06-05 08:35:10 +0100

    Scroll behaviour on Nav component

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
-import { spacing } from '../../utils/globalStyles'
+import { spacing, blackColor, blackTransparentColor } from '../../utils/globalStyles'
 import logo from './beatcampLogo.png'
 import Button from '../button/button'
 
@@ -13,11 +13,15 @@ const Navbar = styled('div')`
   z-index: 1;
   text-align: center;
   color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `
 
 const Logo = styled('img')`
   width: 200px;
   display: inline-block;
+  margin:auto;
 `
 
 const floatRight = css`
@@ -25,12 +29,30 @@ const floatRight = css`
   right: ${spacing}px;
   display: inline-block;
 `
+const scrollStyle = css`
+  display:none;
+`
 
 export default class Nav extends React.Component {
+
+  state = {
+      isTop: true,
+    };
+
+    componentDidMount() {
+      document.addEventListener('scroll', () => {
+        const isTop = window.scrollY < 500;
+        if (isTop !== this.state.isTop) {
+            this.setState({ isTop })
+        }
+      });
+    }
+
+
   render() {
     return(
-      <Navbar>
-        <Logo src={logo} />
+      <Navbar >
+        <Logo src={logo} css={this.state.isTop ? `` : scrollStyle } />
         <div className={floatRight}>
           <Button label="Join a Camp" />
         </div>
