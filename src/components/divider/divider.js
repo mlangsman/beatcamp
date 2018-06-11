3f6c9c8d2f30263e6c0e2cca7efbb69d9e781563
Marc <marc@langsman.com>
2018-06-11 17:01:49 +0100

    Change divider to use global palette object

--- src/components/divider/divider.js
+++ src/components/divider/divider.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import styled, { css } from 'react-emotion';
-import { spacing, backgroundColor, backgroundColorDark } from '../../utils/globalStyles'
+import { spacing, palette } from '../../utils/globalStyles'
 
 const AngledDivider = styled('div')`
   padding-top: ${spacing*5}px;
-  height: 100px;
-  background-color: ${backgroundColorDark};
+  background-color: ${palette.backgroundDark};
   clip-path: polygon(
     0 9vw,
     100% 0%,
