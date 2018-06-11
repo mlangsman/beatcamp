dc700dfbcf6b8cd35873d9dfd42e997d56b60760
Marc <marc@langsman.com>
2018-06-11 17:04:14 +0100

    Add new container component

---
+++ src/components/container/container.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled, { css } from 'react-emotion';
+import { spacing, palette } from '../../utils/globalStyles'
+
+const ContainerLight = styled('div')`
+  padding-left: ${spacing*4}px;
+  padding-right: ${spacing*4}px;
+  background-color: ${palette.backgroundLight};
+`
+
+const ContainerDark = styled('div')`
+  padding-left: ${spacing*4}px;
+  padding-right: ${spacing*4}px;
+  background-color: ${palette.backgroundDark};
+`
+
+export default class Container extends React.Component {
+  render() {
+    if (this.props.style === 'dark')
+    {
+      return(<ContainerDark>{this.props.children}</ContainerDark>);
+    }
+    else if (this.props.style === 'light')
+    {
+      return(<ContainerLight>{this.props.children}</ContainerLight>);
+    }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Typography from 'typography'
-import { css } from 'emotion'
+import styled, { css } from 'react-emotion';
 import { spacing, palette } from '../utils/globalStyles'
 
 import Hero from '../components/hero/hero'
 import Section from '../components/section/section'
 import Divider from '../components/divider/divider'
 import Nav from '../components/nav/nav'
+import Container from '../components/container/container'
 
 const background = css`
   background-color: ${palette.backgroundLight};
 `
 
-const mainContainer = css`
-  margin-left: ${spacing*4}px;
-  margin-right: ${spacing*4}px;
-`
 console.log(spacing);
 
 const IndexPage = () => (
   <div className={background}>
     <Nav />
     <Hero />
-    <div className={mainContainer}>
-      <Section />
-    </div>
+    <Container style="light"><Section /></Container>
     <Divider />
-    <h1>I am a big poo</h1>
+    <Container style="dark"><h1>I am a big poo</h1></Container>
   </div>
 )
 
