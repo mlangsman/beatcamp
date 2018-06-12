f401af7cc237be0df4da804f0070949ad3cc0c9d
Marc <marc@langsman.com>
2018-06-12 09:41:43 +0100

    Add caption font size to global styles

--- src/utils/globalStyles.js
+++ src/utils/globalStyles.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Typography from './typography'
 /* Global style variables */
 
 export const spacing = 30;
@@ -10,3 +10,7 @@ export const palette = {
   nearBlack : '#1A1A1A',
   blackTransparent : 'rgba(26,26,26,0.9)',
 }
+
+/* Additional font sizes */
+
+export const captionSize = Typography.options.baseFontSize.substring(0,Typography.options.baseFontSize.length-2) / Typography.options.scaleRatio*2.5
