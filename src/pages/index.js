a88a4d25d8324b156177172f97bb57925d512aec
Marc <marc@langsman.com>
2018-06-13 16:30:40 +0100

    Added ResponsiveEmbed and video

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,24 @@ import Divider from '../components/divider/divider'
 import Nav from '../components/nav/nav'
 import Container from '../components/container/container'
 import Testimonial from '../components/testimonial/testimonial'
+import ResponsiveEmbed from 'react-responsive-embed'
 
 const background = css`
   background-color: ${palette.backgroundLight};
 `
-
+const border = css`
+  border: 50px solid red;
+`
+const video = css`
+  margin: ${3*spacing}px 5% ${3*spacing}px 5%;
+  border: none;
+  -webkit-box-shadow: 0px 20px 78px 0px rgba(0,0,0,0.3);
+  -moz-box-shadow: 0px 20px 78px 0px rgba(0,0,0,0.3);
+  box-shadow: 0px 20px 78px 0px rgba(0,0,0,0.3);
+`
+const flex = css`
+  display: flex;
+`
 console.log(spacing);
 
 const IndexPage = () => (
@@ -30,6 +43,9 @@ const IndexPage = () => (
 
     <Container style="dark">
       <Testimonial />
+      <div className={video}>
+        <ResponsiveEmbed src='https://www.youtube.com/embed/hNV3NvNDFDM' allowfullscreen />
+      </div>
     </Container>
 
   </div>
