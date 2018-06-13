020118c71d74881ecdfa250961c8a282642be246
Marc <marc@langsman.com>
2018-06-13 17:28:40 +0100

    Adjust spacing in callout box

--- src/components/calloutBox/calloutBox.js
+++ src/components/calloutBox/calloutBox.js
@@ -5,7 +5,7 @@ import { spacing, palette } from '../../utils/globalStyles'
 const CalloutStyle = styled('div')`
   border: solid 15px white;
   background: ${palette.mediumGrey};
-  padding: ${spacing*3}px;
+  padding: ${spacing*3}px ${spacing*3}px ${spacing}px ${spacing*3}px;
 `
 
 const Title = styled('h3')`
